88c3fa9d367be82f8eeafc1eae8b91d51ef941a2
Maria Manriquez <manriquez22446688@gmail.com>
2021-01-21 21:44:23 -0400

    index y ui mejorados

--- ui.js
+++ ui.js
@@ -1,54 +1,68 @@
-import {Products, ProductsManagement} from "./products.js";
-class Ui {
+import Product from "./products.js";
+import ProductManagement from "./products.js";
+class UI {
     constructor() {
-        this.uiName = document.getElementById("name");
-        this.uiDescription = document.getElementById("description");
-        this.uiQuantity = document.getElementById("quantity");
-        this.uiForm = document.getElementById("form-data");
-        this.container = document.getElementById("container-table");
-        this.manager =  new ProductsManagement();
-        let p1 = new Products("Pollo", "Pollo Sofia", 20);
-        let p2 = new Products("Galletas", "Galleras Mabel", 50);
-        let p3 = new Products("Pollo", "Pollo Imba", 50);
-        this.manager.addProducts(p1);
-        this.manager.addProducts(p2);
-        this.manager.addProducts(p3);
+        this.form = document.getElementById("form");
+        this.container = document.getElementById("containertable");
+        console.log("LOAD");
+        this.productmanagement = new ProductManagement();
+
+        this.productmanagement.addProduct("Pollo", "Pollo dofia", 12);
+        this.productmanagement.addProduct("Carne", "Santa cruz", 11);
+        this.productmanagement.addProduct("Leche", "leche Pil", 100);
+        this.productmanagement.addProduct("Tee", "Tee Windsor", 123);
+        this.lisproducts = this.productmanagement.getList();
+        //this.lisproducts = productmanagement.getList();
         this.loadEvents();
-    }
-    loadEvents() {
-        this.uiForm.addEventListener("submit", (e) => {
-            e.preventDefault();
-            this.addProducts(
-                this.uiName.value,
-                this.uiDescription.value,
-                this.uiQuantity.value);
-            this.clearForm();
-        });
-    }
-    clearForm() {
-                this.uiName.value = "";
-                this.uiDescription.value = ""
-                this.uiQuantity.value = ""
+        this.loadTable();
     }
     loadTable() {
-        var html = "";
-        for (var i = 0; i < this.manager.showProducts().length; i++) {
-            html += `
-            <tr>
-                <td scope="row">
-                ${this.manager.showProducts()[i].name}</td>
-                <td>${this.manager.showProducts()[i].description}</td>
-                <td>${this.manager.showProducts()[i].quantity}</td>
-            </tr>`;
-        }
-        this.container.innerHTML = html;
-    }
-    addProducts(name, description, quantity) {
-        let p1 = new Products(name, description, quantity);
-        this.manager.addProducts(p1);
-        this.loadTable();
+        this.html = `
+        <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+        `;
+        for (var i = 0; i < this.lisproducts.length; i++) {
+            this.html += `<tr>
+            <th scope="row">${i}</th>
+            <td>${this.lisproducts[i].getName()}</td>
+            <td>${this.lisproducts[i].getDescription()}</td>
+            <td>${this.lisproducts[i].getQuantity()}</td>
+            <td><button type="button" class="btn btn-danger" ids="${i}">Borrar</button></td>
+          </tr>`;
 
+        }
+        this.html += `
+        </tbody>
+              </table>
+        `;
+        this.container.innerHTML = this.html;
+        var buttons = document.querySelectorAll("button");
+        for (var i = 0; i < buttons.length; i++){
+            buttons[i].addEventListener("click", (e) => {
+                var product = this.lisproducts[Number(e.target.getAttribute("ids"))];
+                this.productmanagement.removeProducts(product);
+                this.loadTable();
+            });
+        }
+    }
+    loadEvents() {
+        this.form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            var name = document.getElementsByName("name");
+            var description = document.getElementsByName("description");
+            var quantity = document.getElementsByName("quantity");
+
+            this.productmanagement.addProduct(name[0].value, description[0].value, quantity[0].value);
+            this.loadTable();
+        })
     }
 }
-let ui = new Ui();
-ui.loadTable();
+var init = new UI();
